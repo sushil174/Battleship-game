b8a441ebce40e437b65346386b2db38afe77534c
sushil174 <sushilkoregave123@gmail.com>
2024-09-11 01:51:32 +0530

    ship manual placing

--- src/DOM/PlaceShip.js
+++ src/DOM/PlaceShip.js
@@ -5,7 +5,59 @@ class PlaceShip {
         this.gameBoard = gameBoard
         this.cells = []
         this.ground = ground
+        this.currentShipSize = 0
+        this.shipButtons()
     }
+
+    shipButtons() {
+        const buttonContainer = document.createElement('div')
+        const container = document.querySelector('.container')
+        const button_4 = document.createElement('button')
+        button_4.dataset.value = 4
+        
+        button_4.addEventListener('click', () => {
+            this.currentShipSize = 4
+            this.cells = []
+            this.display()
+        })
+
+
+        const button_3 = document.createElement('button')
+        button_3.dataset.value = 3
+        button_3.addEventListener('click', () => {
+            this.currentShipSize = 3
+            this.cells = []
+            this.display()
+        })
+
+        const button_2 = document.createElement('button')
+        button_2.dataset.value = 2
+        button_2.addEventListener('click', () => {
+            this.currentShipSize = 2
+            this.cells = []
+            this.display()
+        })
+
+        const button_1 = document.createElement('button')
+        button_1.dataset.value = 1
+        button_1.addEventListener('click', () => {
+            this.currentShipSize = 1
+            this.cells = []
+            this.display()
+        })
+
+
+        button_4.textContent = "++++"
+        button_3.textContent = "+++"
+        button_2.textContent = "++"
+        button_1.textContent = "+"
+        buttonContainer.append(button_4)
+        buttonContainer.append(button_2)
+        buttonContainer.append(button_3)
+        buttonContainer.append(button_1)
+        container.append(buttonContainer)
+    }
+
 
     display() {
         this.ground.textContent = ''
@@ -23,20 +75,40 @@ class PlaceShip {
                 }
                 this.ground.append(cell)
                 row.push(cell)
-                this.test(cell)
+                this.test(cell,this.currentShipSize)
                 
             }
             this.cells.push(row)
         }
     }
 
-    test(cell) {
-        const ship = [[0,0],[0,1],[0,2],[0,3]]
+    test(cell,shipSize) {
+        // const ship = [[0,0],[1,0],[2,0],[3,0]]
+        let shipCoords = []
+        for(let i = 0; i < shipSize; i++) {
+            let position = [0,i]
+            shipCoords.push(position)
+        }
+
         let coords;
+        cell.addEventListener('click', () => {
+            if(this.gameBoard.validate(coords)) {
+                const ship = new Ship(shipSize)
+                this.gameBoard.placeShip(ship,coords)
+                for(let [x,y] of coords) {
+                    this.cells[x][y].style.backgroundColor = 'blue'
+                }  
+                this.currentShipSize = 0
+                this.cells = []
+                this.display()
+            }
+        })
+
+
         cell.addEventListener('mouseenter', () => {
             const x = parseInt(cell.dataset.x)
             const y = parseInt(cell.dataset.y)
-            coords = ship.map((a)=> {
+            coords = shipCoords.map((a)=> {
                 return [a[0] + x, a[1] + y]
             })
             let result = this.gameBoard.validate(coords) 
@@ -56,47 +128,8 @@ class PlaceShip {
             }   
         })
 
-        cell.addEventListener('click', () => {
-            if(this.gameBoard.validate(coords)) {
-                const ship = new Ship(4)
-                this.gameBoard.placeShip(ship,coords)
-                // this.display()
-            }
-        })
     }
-    // const domCells = playerBoard.display()
-    // let clicked = false
-    // const cells = document.querySelectorAll('.cell')
-    // cells.forEach( cell => {
-    //     let x = parseInt(cell.dataset.x)
-    //     let y = parseInt(cell.dataset.y)
-    //     cell.addEventListener('mouseenter', (e)=> {
-    //         if(!clicked) {
-    //             domCells[x][y].style.backgroundColor = 'yellow'
-    //             domCells[x][y + 1].style.backgroundColor = 'yellow'
-    //             domCells[x][y + 2].style.backgroundColor = 'yellow'
-    //             domCells[x][y + 3].style.backgroundColor = 'yellow'
-    //         }
-                
-    //     })
-
-        // cell.addEventListener('mouseleave', (e)=> {
-        //     if(!clicked) {
-        //         domCells[x][y].style.backgroundColor = 'lightblue'
-        //         domCells[x][y + 1].style.backgroundColor = 'lightblue'
-        //         domCells[x][y + 2].style.backgroundColor = 'lightblue'
-        //         domCells[x][y + 3].style.backgroundColor = 'lightblue'
-        //     }
-        // })
-
-    //     cell.addEventListener('click', () => {
-    //         clicked = true
-    //         domCells[x][y].style.backgroundColor = 'yellow'
-    //         domCells[x][y + 1].style.backgroundColor = 'yellow'
-    //         domCells[x][y + 2].style.backgroundColor = 'yellow'
-    //         domCells[x][y + 3].style.backgroundColor = 'yellow'
-    //     })
-    // })
+ 
 }
 
 export default PlaceShip
